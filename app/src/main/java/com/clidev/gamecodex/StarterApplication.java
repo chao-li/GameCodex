44149e796047f61a36dfd36980475a99b7e31aeb
Chao Li <chao.li.mobile.dev@gmail.com>
2018-08-14 04:47:53 +1000

    Added leak canary

--- app/src/main/java/com/clidev/gamecodex/StarterApplication.java
+++ app/src/main/java/com/clidev/gamecodex/StarterApplication.java
@@ -2,6 +2,8 @@ package com.clidev.gamecodex;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import timber.log.Timber;
 
 public class StarterApplication extends Application {
@@ -9,6 +11,15 @@ public class StarterApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                // This process is dedicated to LeakCanary for heap analysis.
+                // You should not init your app in this process.
+                return;
+            }
+            LeakCanary.install(this);
+        }
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
