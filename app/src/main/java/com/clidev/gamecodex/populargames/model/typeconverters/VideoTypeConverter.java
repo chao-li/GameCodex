640215fd98a25263c7d8e8ce432312a2fff7ba32
Chao Li <chao.li.mobile.dev@gmail.com>
2018-08-15 20:29:47 +1000

    Video type converter added

---
+++ app/src/main/java/com/clidev/gamecodex/populargames/model/typeconverters/VideoTypeConverter.java
@@ -0,0 +1,31 @@
+package com.clidev.gamecodex.populargames.model.typeconverters;
+
+import com.clidev.gamecodex.populargames.model.modeldata.Video;
+
+public class VideoTypeConverter {
+
+    public static String toString(Video video) {
+        if (video != null) {
+            String name = video.getName();
+            String videoId = video.getVideoId();
+
+            String string = name + "," + videoId;
+            return string;
+        }
+        return null;
+    }
+
+    public static Video toVideo(String string) {
+        if (string != null) {
+            String[] split = string.split(",");
+
+            String name = split[0];
+            String videoId = split[1];
+
+            Video video = new Video(name, videoId);
+            return video;
+        }
+        return null;
+    }
+
+}
