2ce01c3022590fc4982c7812f2d0072fac64a0e1
Chao Li <chao.li.mobile.dev@gmail.com>
2018-08-18 04:07:19 +1000

    Game detail screen, loaded title, release date, summary

--- app/src/main/java/com/clidev/gamecodex/gamedetails/view/GameDetailsFragment.java
+++ app/src/main/java/com/clidev/gamecodex/gamedetails/view/GameDetailsFragment.java
@@ -8,16 +8,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.clidev.gamecodex.R;
 import com.clidev.gamecodex.gamedetails.view_model.GameDetailsViewModel;
 import com.clidev.gamecodex.gamedetails.view_model.GameDetailsViewModelFactory;
 import com.clidev.gamecodex.populargames.model.modeldata.Game;
 import com.clidev.gamecodex.populargames.view.PopularGamesActivity;
+import com.clidev.gamecodex.utilities.ImageUrlEditor;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import timber.log.Timber;
 
 public class GameDetailsFragment extends Fragment {
@@ -25,10 +36,27 @@ public class GameDetailsFragment extends Fragment {
     private Long mId;
     private GameDetailsViewModel mGameDetailsViewModel;
 
+    // View Fields
+    @BindView(R.id.game_detail_trailer_rv) RecyclerView mTrailerRv;
+    @BindView(R.id.game_detail_cover) ImageView mCoverImage;
+    @BindView(R.id.game_detail_title) TextView mTitleText;
+    @BindView(R.id.game_detail_developer) TextView mDeveloperText;
+    @BindView(R.id.game_detail_platform) TextView mPlatformText;
+    @BindView(R.id.game_detail_rating) TextView mRatingText;
+    @BindView(R.id.game_detail_release_date) TextView mReleaseDateText;
+    @BindView(R.id.game_detail_summary_title) TextView mSummaryTitle;
+    @BindView(R.id.game_detail_summary_detail) TextView mSummaryText;
+    @BindView(R.id.game_detail_loading_bar) ProgressBar mLoadingBar;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_game_details, container, false);
+
+        ButterKnife.bind(this, rootView);
+
+        clearViews();
 
         Intent intent = getActivity().getIntent();
         mId = intent.getLongExtra(PopularGamesActivity.SELECTED_GAME_ID, 0);
@@ -43,6 +71,8 @@ public class GameDetailsFragment extends Fragment {
     }
 
     private void loadGameData() {
+        mLoadingBar.setVisibility(View.VISIBLE);
+
         GameDetailsViewModelFactory factory = new GameDetailsViewModelFactory(mId);
 
         mGameDetailsViewModel = ViewModelProviders.of(this, factory).get(GameDetailsViewModel.class);
@@ -53,10 +83,115 @@ public class GameDetailsFragment extends Fragment {
                 Timber.d("LiveData change observed");
 
                 Timber.d("Game name: " + game.getName());
+
+                mLoadingBar.setVisibility(View.INVISIBLE);
+
+                setTrailerRecyclerView(game);
+
+                setCoverPoster(game);
+
+                setGameTitle(game);
+
+                setDeveloperName(game);
+
+                setPlatforms(game);
+
+                setFirstReleaseDate(game);
+
+                setAggregatedRating(game);
+
+                setSummary(game);
+
             }
         });
+    }
 
+    private void setTrailerRecyclerView(Game game) {
+    }
 
+    private void setCoverPoster(Game game) {
+        String imageUrl = "";
+        if (game.getCover() != null) {
+            if (game.getCover().getUrl() != null) {
+                imageUrl = game.getCover().getUrl();
+                imageUrl = ImageUrlEditor.getBigCoverUrl(imageUrl);
+            }
+        }
+
+        RequestOptions options = new RequestOptions();
+        options.centerCrop();
+        options.override(150,190);
+        options.placeholder(R.drawable.image_loading);
+
+        Glide.with(getContext())
+                .load(imageUrl)
+                .apply(options)
+                .into(mCoverImage);
+    }
+
+    private void setGameTitle(Game game) {
+        String title = "Title of this game isn't available!";
+        if (game.getName() != null) {
+            title = game.getName();
+        }
+        mTitleText.setText(title);
+    }
+
+    private void setDeveloperName(Game game) {
+        String developer = "";
+        if (game.getDevelopers() != null) {
+            List<Long> developerIds = game.getDevelopers();
+
+            // TODO: make retrofit call to get the developer names
+        }
 
     }
+
+    private void setPlatforms(Game game) {
+        String platform = "";
+        if (game.getPlatforms() != null) {
+            List<Integer> platformIds = game.getPlatforms();
+
+            // TODO: make retrofit call to get platform names
+        }
+    }
+
+    private void setFirstReleaseDate(Game game) {
+        String releaseDate = "";
+        if (game.getFirstReleaseDate() != null ) {
+            Long releaseDateInSec = game.getFirstReleaseDate();
+
+            // TODO: convert to human format
+        }
+    }
+
+    private void setAggregatedRating(Game game) {
+        String rating = "";
+        if (game.getAggregatedRating() != null) {
+            Double ratingNumber = game.getAggregatedRating();
+            rating = String.format("%.2f", ratingNumber) + "/100";
+        }
+        mRatingText.setText(rating);
+    }
+
+    private void setSummary(Game game) {
+        String summary = "";
+        if (game.getSummary() != null) {
+            summary = game.getSummary();
+        }
+        mSummaryTitle.setText("Summary");
+        mSummaryText.setText(summary);
+    }
+
+    private void clearViews() {
+        mSummaryTitle.setText("");
+        mSummaryText.setText("");
+        mTitleText.setText("");
+        mDeveloperText.setText("");
+        mRatingText.setText("");
+        mPlatformText.setText("");
+        mReleaseDateText.setText("");
+
+    }
+
 }
